456c38cc75e83dc756a248eae17b18d873a60635
KarthikJR <karthikjrao69@gmail.com>
2024-12-07 14:57:29 +0530

    feat: add authentication page

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from "@/components/login-form";
+import React from "react";
+
+function Page() {
+	return (
+		<div className="flex h-screen w-full items-center justify-center px-4">
+			<LoginForm />
+		</div>
+	);
+}
+
+export default Page;
---
+++ components/login-form.tsx
@@ -0,0 +1,91 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+export function LoginForm() {
+	const loginSchema = z.object({
+		username: z.string().min(1, {
+			message: "Please enter an username",
+		}),
+		password: z.string().min(1, {
+			message: "Please enter a password",
+		}),
+	});
+
+	const form = useForm<z.infer<typeof loginSchema>>({
+		resolver: zodResolver(loginSchema),
+		defaultValues: {
+			username: "",
+			password: "",
+		},
+	});
+
+	const onSubmit = (values: z.infer<typeof loginSchema>) => {
+		console.log(values);
+	};
+
+	return (
+		<Card className="mx-auto max-w-sm">
+			<CardHeader>
+				<CardTitle className="text-2xl">Login</CardTitle>
+				<CardDescription>
+					Enter your email below to login to your account
+				</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-4">
+						<FormField
+							control={form.control}
+							name="username"
+							render={({ field }) => (
+								<FormItem className="grid gap-2">
+									<FormLabel>Username</FormLabel>
+									<FormControl>
+										<Input placeholder="Username" {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="password"
+							render={({ field }) => (
+								<FormItem className="grid gap-2">
+									<FormLabel>Password</FormLabel>
+									<FormControl>
+										<Input type="password" placeholder="Password" {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<Button type="submit">Submit</Button>
+					</form>
+				</Form>
+			</CardContent>
+		</Card>
+	);
+}
